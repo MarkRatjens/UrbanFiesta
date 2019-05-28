4553c7b6a5401abc9dff06ef1fb180baf98adb56
Mark Ratjens <mark@ratjens.com>
2019-05-28 12:47:59 +1000

    use a prime count to make the waitlist look bigger than it is

--- credit_registration/models/credit_registration.rb
+++ credit_registration/models/credit_registration.rb
@@ -1,2 +1,17 @@
 class CreditRegistration < ActiveRecord::Base
+  def self.primer
+    51723
+  end
+
+  def self.primer_count
+    count + primer
+  end
+
+  def waitlist_position
+    id + self.class.primer
+  end
+
+  def referral_code
+    "#{email.upcase.delete('AEIOU._@')[0..4]}#{self.class.count + 1}".delete('0')
+  end
 end
